5f560968583f5df42e62f349e10517482908b11b
ThevinduKevin <thevindukevin@gmail.com>
2024-06-24 13:10:48 +0530

    connect to the database

---
+++ src/ConnectionClass.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+
+public class ConnectionClass {
+
+    Connection con;
+    Statement stm;
+
+    ConnectionClass(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ams","root","");
+            stm = con.createStatement();
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
